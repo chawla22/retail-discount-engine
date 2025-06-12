3c3dc45c8bfe365ac00f8b24ebe8f083ca3f342c
chawla22 <chawla22@usf.edu>
2025-06-13 01:22:07 +0530

    created the javascript file and linked it to the html

---
+++ cc_4.js
@@ -0,0 +1,9 @@
+//  In this challenge, you’ll build a basic retail discount engine using JavaScript conditionals and loops. This tool will simulate pricing decisions based on customer types, product inventory levels, and promotional categories.
+// You will practice decision-making using if, else if, and switch statements, then use loops to cycle through customer orders and inventory updates. The engine must evaluate multiple pricing rules and apply dynamic business logic across an inventory dataset.
+// Important Note
+// Test your logic using console.log(). Use comments to explain your control structures and avoid redundant conditions. Use break statements to avoid switch case fall-through.
+
+
+// Step 1: Create a new JavaScript file called cc_4.js and link it to an index.html file.
+
+console.log("Retail Discount Engine  - Challenge 4");
